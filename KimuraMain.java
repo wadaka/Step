19562a94c40e5d7c8a0551a8c22526aed1c27cf6
wadaka <erou1227@yahoo.co.jp>
2022-03-28 14:00:21 +0900

    バッテン図形作成

--- KimuraMain.java
+++ KimuraMain.java
@@ -5,8 +5,26 @@ public class KimuraMain{
 		int input = new Scanner(System.in).nextInt();
 
 		for(int i=0;i<input;i++){
-			for(int j=0;j<i+1;j++){
+			for(int j=0;j<=i;j++){
 				System.out.print("*");
+			}
+			System.out.println();
+		}
+		//逆
+		System.out.println();
+		System.out.println("//逆");
+		System.out.println();
+
+		for(int i=0;i<input;i++){
+			for(int j=0;j<input;j++){
+				System.out.print(j<i? " ":"*");
+			}
+			System.out.println();
+		}
+		//x
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				System.out.print((j+i)%2==0 ? "×":" ");
 			}
 			System.out.println();
 		}
